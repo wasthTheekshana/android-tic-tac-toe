adafaaba4d184fbe62b7241de6855727777923a7
Theekshana Hariscandra <47950733+wasthTheekshana@users.noreply.github.com>
2021-09-05 16:43:08 +0530

    add two activities NameActivity,TwoNameActivity

--- app/src/main/java/com/wasthdev/my_tic_tac_toe/MainActivity.java
+++ app/src/main/java/com/wasthdev/my_tic_tac_toe/MainActivity.java
@@ -3,13 +3,34 @@ package com.wasthdev.my_tic_tac_toe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn_1,btn_2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        btn_1 = findViewById(R.id.btn_1);
+        btn_2 = findViewById(R.id.btn_2);
+
+        btn_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        btn_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/wasthdev/my_tic_tac_toe/TwoNameActivity.java
@@ -0,0 +1,14 @@
+package com.wasthdev.my_tic_tac_toe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TwoNameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_two_name);
+    }
+}
